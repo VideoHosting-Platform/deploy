92138abd504bf2dea5934d8db0821ec3bcf70099
redblood-pixel <danka2202@yandex.ru>
2025-04-15 21:06:20 +0300

    run.sh updated

--- run.sh
+++ run.sh
@@ -10,15 +10,28 @@ check_status() {
 }
 
 minikube start
+kubectl create namespace minio
 kubectl apply -f minio-deployment.yaml
 kubectl create secret generic minio-credentials \
   --from-literal=accesskey=minioadmin \
   --from-literal=secretkey=minioadmin \
   --from-literal=endpoint=
+
+# Добавляем репозиторий Argo
+helm repo add argo https://argoproj.github.io/argo-helm
+
+# Обновляем репозитории
+helm repo update
+
+helm install argo-workflows argo/argo-workflows \
+  --namespace argo \
+  --create-namespace \
+  --set "server.extraArgs={--auth-mode=server}"
+
 nohup kubectl -n argo port-forward svc/argo-workflows-server 2746:2746 &
 check_status "Argo Workflows Server (port 2746)"
 
-nohup kubectl port-forward -n default svc/minio 9000:9000 9001:9001  > /dev/null 2>&1 &
+nohup kubectl port-forward -n minio svc/minio 9000:9000 9001:9001  > /dev/null 2>&1 &
 check_status "MinIO (ports 9000, 9001)"
 
 nohup kubectl port-forward -n kube-system svc/registry 5000:80 &
