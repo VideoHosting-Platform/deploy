97c87cff50f599afe3939a6a6371f32e368044ae
AndrewSmooth <gladkiy.a2004@gmail.com>
2025-04-15 20:04:08 +0300

    shell scripts

---
+++ run.sh
@@ -0,0 +1,30 @@
+# запускает minikube, minio, argo, registry и дашборды для них
+
+check_status() {
+  if [ $? -eq 0 ]; then
+    echo "✅ $1 запущен успешно!"
+  else
+    echo "❌ Ошибка при запуске $1!"
+    exit 1
+  fi
+}
+
+minikube start
+kubectl apply -f minio-deployment.yaml
+kubectl create secret generic minio-credentials \
+  --from-literal=accesskey=minioadmin \
+  --from-literal=secretkey=minioadmin \
+  --from-literal=endpoint=
+nohup kubectl -n argo port-forward svc/argo-workflows-server 2746:2746 &
+check_status "Argo Workflows Server (port 2746)"
+
+nohup kubectl port-forward -n default svc/minio 9000:9000 9001:9001  > /dev/null 2>&1 &
+check_status "MinIO (ports 9000, 9001)"
+
+nohup kubectl port-forward -n kube-system svc/registry 5000:80 &
+check_status "Registry (port 5000)"
+
+nohup minikube dashboard &
+check_status "Minikube Dashboard"
+
+echo "Все сервисы запущены в фоне. 🚀"
