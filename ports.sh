a6b6cbf829f47104efab2e0600ee2f0fa63618c9
AndrewSmooth <gladkiy.a2004@gmail.com>
2025-04-17 15:52:55 +0300

    fastapi image update

--- ports.sh
+++ ports.sh
@@ -25,15 +25,17 @@ if ! command -v mc &> /dev/null; then
 nohup kubectl -n argo port-forward svc/argo-workflows-server 2746:2746 &
 check_status "Argo Workflows Server (port 2746)"
 
+sleep 5
 nohup kubectl port-forward -n minio svc/minio 9000:9000 9001:9001  > /dev/null 2>&1 &
 check_status "MinIO (ports 9000, 9001)"
+sleep 3
 
 nohup kubectl port-forward -n kube-system svc/registry 5000:80 &
 check_status "Registry (port 5000)"
 
 # Создание бакета videos и настройка нотификаций
 export PATH=$PATH:$HOME/minio-binaries/
-mc alias set minio http://localhost:9000 minioadmin minioadmin
+mc alias set minio http://minio.minio.svc.cluster.local:9000 minioadmin minioadmin
 mc mb minio/videos
 mc mb minio/video-files
 mc admin config set minio notify_webhook:service endpoint="http://fastapi-service.default.svc.cluster.local:8000/webhook"
